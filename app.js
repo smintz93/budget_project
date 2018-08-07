7093071690adfc2f9fdaf16999b43a247b20f37c
Sam <sammintz1993@gmail.com>
2018-08-07 12:44:30 -0500

    ui controller and app controller

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const budgetController = (function() {
 	const x = 23;
-
 	const add = function(a){
 		return x + a
 	}
@@ -10,4 +9,18 @@ const budgetController = (function() {
 			console.log(add(b));
 		}
 	}
+})();
+// this gets called right away bc of line 13
+// works because of closure. inner function always has access to variables even after return statement
+
+
+const UIController = (function() {
+
+	// some code
+
+})();
+
+const dataController = (function() {
+
+	// some code 
 })();
