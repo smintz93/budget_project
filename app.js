2e17ffa9c5dd552e09794cab93feb5d18d8fdee7
Sam <sammintz1993@gmail.com>
2018-08-08 12:08:34 -0500

    event handler for enter

--- app.js
+++ app.js
@@ -19,7 +19,26 @@ const UIController = (function() {
 const controller = (function(budgetCtrl, UICtrl) {
 
 	document.querySelector(".add__btn").addEventListener("click", function(){
-		
+
+		// 1. Get input field data
+
+		// 2. Add item to the Budget Controller 
+
+		// 3. Add the item to UI 
+
+		// 4. Calc budget 
+
+		// 5. Display budget on UI
+	})
+
+	// If user hits 'enter' --> keycode 107
+	document.addEventListener("keypress", function(e){
+
+		if(e.keyCode === 13){
+			console.log('ENTER was pressed')
+		} else {
+			console.log("...")
+		}
 	})
 
 })(budgetController, UIController);
