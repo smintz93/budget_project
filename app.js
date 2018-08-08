c9793d91c006c9b3bd210f285596f0b9365fa22d
Sam <sammintz1993@gmail.com>
2018-08-08 11:59:25 -0500

    event handler for checkbutton

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
-const budgetController = (function() {
-	const x = 23;
-	const add = function(a){
-		return x + a
-	}
 
-	return {
-		publicTest: function(b){
-			return add(b);
-		}
-	}
+// BUDGET CONTROLLER 
+const budgetController = (function() {
+
+
 })();
 // this gets called right away bc of line 13
 // works because of closure. inner function always has access to variables even after return statement
 
 
+// UI CONTROLLER 
 const UIController = (function() {
 
 	// some code
 
 })();
 
+// GLOBAL APP CONTROLLER 
 const controller = (function(budgetCtrl, UICtrl) {
 
-	const z = budgetCtrl.publicTest(5);
-
-	return {
-		anotherPublic: function(){
-			console.log(z);
-		}
-	}
+	document.querySelector(".add__btn").addEventListener("click", function(){
+		
+	})
 
 })(budgetController, UIController);
