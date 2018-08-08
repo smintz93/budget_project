dd4478a23196ba441424f1136501b0ab1abb24f4
Sam <sammintz1993@gmail.com>
2018-08-08 12:59:27 -0500

    data strucuture for all data coming in

--- app.js
+++ app.js
@@ -9,12 +9,21 @@ const budgetController = (function() {
 	};
 
 	const Income = function(id, description, value) {
-	this.id = id;
-	this.description = description;
-	this.value = value;
+		this.id = id;
+		this.description = description;
+		this.value = value;
 	};
 
-
+	const data = {
+		 allItems: {
+		 	exp: [],
+		 	inc:[]
+		 },
+		 totals: {
+		 	exp: 0,
+		 	inc: 0
+		 }
+	}
 
 })();
 
