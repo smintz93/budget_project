bdcfdd9dfb0fd1ee90fee0361b9283441b76057c
Sam <sammintz1993@gmail.com>
2018-08-09 09:48:24 -0500

    saved html for inc and exp in variables to use

--- app.js
+++ app.js
@@ -78,6 +78,24 @@ const UIController = (function() {
 			 value: document.querySelector(DOMstrings.inputValue).value
 			}	
 		},
+
+		addListItem: function(obj, type){
+			// Create HTML string with palceholder text
+			let html, newHtml;
+
+			if(type === 'inc') {
+				html =  '<div class="item clearfix" id="income-%id%"> <div class="item__description"> %description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div></div>'
+			} else if(type === 'exp') {
+				html = '<div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">- %value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+			}
+
+			// Replace pholder text with real data 
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+
+			// Insert HTML into DOM 
+		},
 		// Public so it can be passed to Global App controller 
 		getDOMstrings: function(){
 			return DOMstrings;
